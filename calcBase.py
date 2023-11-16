46618b98500c8cbbf9ecefe40f85fc3cb08313b4
Paul Gaudeaux <phvcgdx@gmail.com>
2023-11-16 23:42:53 +0100

    refac to use an AST

--- calcBase.py
+++ calcBase.py
@@ -25,7 +25,7 @@ tokens = [
     'LPAREN',       'RPAREN',      'AND', 
     'OR',           'SEMI',        'NAME', 
     'EQUALS',       'INF',         'SUP', 
-    'INFEQ',        'SUPEQ',       'INC', 
+    'INFEQ',        'SUPEQ',
     'COMMENTLINE',  'COMMENTBLOCK','STRING'
 ] + list(reserved.values())
 
@@ -75,68 +75,55 @@ lex.lex()
 
 names = {}
 
-def p_start(p):
-    'start : expression'
-    p[0] = ('START', p[1])
-    print('arbre: ', p[0])
-    printTreeGraph(p[1])
-    print('calc > ', eval(p[1]))
-
 def p_block(p):
-    '''block : statement SEMI block
+    '''block : block statement SEMI
              | statement SEMI
              | COMMENTLINE
              | COMMENTBLOCK'''
+    
+    if(len(p) == 4): # had to put the block on the left to execute the insturctions in the right order
+        eval(p[2])
+    else:
+        eval(p[1])
 
-
-def p_assign(p):
+def p_statement_assign(p):
     'statement : NAME EQUALS expression'
-    # names[p[1]] = p[3]
     p[0] = ('assign', p[1], p[3])
 
 def p_statement_expr(p):
     'statement : PRINT LPAREN expression RPAREN'
-    # print('calc > ', p[3])
     p[0] = ('print', p[3])
 
 def p_statement_add_one(p):
     'statement : NAME PLUS PLUS'
-    # names[p[1]] += 1
     p[0] = ('incrementone', p[1])
 
 def p_statement_sub_one(p):
     'statement : NAME MINUS MINUS'
-    # names[p[1]] -= 1
     p[0] = ('decrementone', p[1])
 
 def p_statement_add(p):
     'statement : NAME PLUS EQUALS expression'
-    # names[p[1]] += p[4]
     p[0] = ('increment', p[1], p[4])
 
 def p_statement_sub(p):
     'statement : NAME MINUS EQUALS expression'
-    # names[p[1]] -= p[4]
     p[0] = ('decrement', p[1], p[4])
 
 def p_expression_binop_inf(p):
     'expression : expression INF expression'
-    # p[0] = p[1] < p[3]
     p[0] = ('smaller', p[1], p[3])
 
 def p_expression_binop_sup(p):
     'expression : expression SUP expression'
-    # p[0] = p[1] > p[3]
     p[0] = ('greater', p[1], p[3])
 
 def p_expression_binop_infeq(p):
     'expression : expression INFEQ expression'
-    # p[0] = p[1] <= p[3]
     p[0] = ('smallerequal', p[1], p[3])
 
 def p_expression_binop_supeq(p):
     'expression : expression SUPEQ expression'
-    # p[0] = p[1] >= p[3]
     p[0] = ('greaterequal', p[1], p[3])
 
 def p_expression_binop_plus(p):
@@ -146,26 +133,41 @@ def p_expression_binop_plus(p):
 
 def p_expression_binop_times(p):
     'expression : expression TIMES expression'
-    p[0] = ('mutiply', p[1], p[3])
+    p[0] = ('multiply', p[1], p[3])
 
 def p_expression_binop_divide_and_minus(p):
     '''expression : expression MINUS expression
 				| expression DIVIDE expression'''
-    # if p[2] == '-': p[0] = p[1] - p[3]
-    # else : p[0] = p[1] / p[3]
 
     if p[2] == '-': p[0] = ('substract', p[1], p[3])
     else : p[0] = ('divide', p[1], p[3])
 
 def p_expression_binop_and(p):
     'expression : expression AND expression'
-    # p[0] = p[1] and p[3]
     p[0] = ('and', p[1], p[3])
 
 def p_expression_binop_or(p):
     'expression : expression OR expression'
-    # p[0] = p[1] or p[3]
     p[0] = ('or', p[1], p[3])
+    
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expression_string(p):
+    'expression : STRING'
+    p[0] = p[1].replace("\"", "")
+
+def p_expression_name(p):
+    'expression : NAME'
+    p[0] = ('get', p[1])
+
+def p_error(p):
+    print("Syntax error at '%s'" % p.value)
 
 def eval(t):
     if type(t) == int:
@@ -174,36 +176,47 @@ def eval(t):
     if type(t) == tuple:
         if t[0] == 'add':
             return eval(t[1]) + eval(t[2])
-        if t[0] == 'substract':
+        elif t[0] == 'substract':
             return eval(t[1]) - eval(t[2])
-        if t[0] == 'multiply':
+        elif t[0] == 'multiply':
             return eval(t[1]) * eval(t[2])
-        if t[0] == 'divide' :
-            if t[2] != 0 :
+        elif t[0] == 'divide':
+            if eval(t[2]) != 0:
                 return eval(t[1]) / eval(t[2])
-        # TODO : handle error when dividing by 0
-    
-def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
-    # p[0] = p[2]
-    p[0] = p[2]
-
-def p_expression_number(p):
-    'expression : NUMBER'
-    # p[0] = p[1]
-    p[0] = p[1]
-
-def p_expression_string(p):
-    'expression : STRING'
-    # p[0] = p[1].replace("\"", "")
-    p[0] = p[1].replace("\"", "")
-
-def p_expression_name(p):
-    'expression : NAME'
-    p[0] = names[p[1]]
-
-def p_error(p):
-    print("Syntax error at '%s'" % p.value)
+            else:
+                print("Error: Division by zero")
+                return None
+        elif t[0] == 'print':
+            print_value = eval(t[1])
+            print("print >", print_value)
+            return print_value
+        elif t[0] == 'smaller':
+            return eval(t[1]) < eval(t[2])
+        elif t[0] == 'greater':
+            return eval(t[1]) > eval(t[2])
+        elif t[0] == 'smallerequal':
+            return eval(t[1]) <= eval(t[2])
+        elif t[0] == 'greaterequal':
+            return eval(t[1]) >= eval(t[2])
+        elif t[0] == 'and':
+            return eval(t[1]) and eval(t[2])
+        elif t[0] == 'or':
+            return eval(t[1]) or eval(t[2])
+        elif t[0] == 'assign':
+            names[t[1]] = eval(t[2])
+        elif t[0] == 'incrementone':
+            names[t[1]] += 1
+        elif t[0] == 'decrementone':
+            names[t[1]] -= 1
+        elif t[0] == 'increment':
+            names[t[1]] += eval(t[2])
+        elif t[0] == 'decrement':
+            names[t[1]] -= eval(t[2])
+        elif t[0] == 'get':
+            return names.get(t[1])
+    else:
+        print("Unknown expression type:", t)
+        return None
 
 import ply.yacc as yacc
 yacc.yacc()
